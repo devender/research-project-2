12c379f21f0f859492074f9177f0b6ae987838cf
Devender <devender.gollapally@gmail.com>
2016-03-29 23:08:46 -0700

    work

--- scratch.R
+++ scratch.R
@@ -23,9 +23,29 @@ if(!file.exists("data/StormData.csv")){
 }
 
 storm_data <- fread("data/StormData.csv",sep=",",header=TRUE,select=c("EVTYPE",
-                                                                    "FATALITIES",
-                                                                    "INJURIES",
-                                                                    "PROPDMG",
-                                                                    "PROPDMGEXP",
-                                                                    "CROPDMG",
-                                                                    "CROPDMGEXP"))
+"FATALITIES","INJURIES","PROPDMG","PROPDMGEXP","CROPDMG","CROPDMGEXP"))
+
+storm_data <- transform(storm_data,EVTYPE = factor(EVTYPE))
+
+str(storm_data)
+
+expand_expo<-function(char){
+    if(char == "M" | char == "m"){
+        return(1e+06)
+    }else if(char == "K" | char == "k"){
+        return(1e+03)
+    }else if(char=="B" | char == "b"){
+        return(1e+09)
+    }else if(char=="H" | char == "h"){
+        return(1e+02)
+    }else if(char=="" | char=="+" | char=="?" | char == "-"){
+        return(1)
+    }else{
+        return(as.numeric(char))
+    }
+}
+v_expand_expo<-Vectorize(expand_expo, SIMPLIFY = TRUE)
+
+storm_data<-storm_data %>%
+    mutate(cropDamage = CROPDMG * v_expand_expo(CROPDMGEXP)) %>%
+    mutate(propDamage = PROPDMG * v_expand_expo(PROPDMGEXP))
